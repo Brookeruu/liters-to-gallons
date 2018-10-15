2a4f6104501d858a14abc6e2c0b76c3ed267c385
Brooke Perry and Tanvi Garg <brooke@>
2018-10-15 14:23:57 -0700

    create gallon to liter and liter to gallon convertor for cookie recipe

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+function getLiters(gallons) {
+var result = (gallons * 3.785);
+return result;
+}
+
+
+function getGallon(liters) {
+var result = (liters * 0.264);
+return result;
+}
+
+ alert(getGallon(2));
